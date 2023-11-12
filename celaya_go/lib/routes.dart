ad175d19767e4f7da5d76a860c556b61cf2b40e4
CharlChief <salazar.charlie2000@gmail.com>
2023-11-12 13:15:46 -0600

    Doy por inaugurado los días de no dormir.
    llamen a diosito, ahhhhhh!

---
+++ celaya_go/lib/routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+Map<String,WidgetBuilder> getRouters(){
+  return{
+    //'/namerout' : (BuildContext context) => classname(),
+    //'/namerout' : (BuildContext context) => classname(),
+    //'/namerout' : (BuildContext context) => classname(),
+    //'/namerout' : (BuildContext context) => classname(),
+    //'/namerout' : (BuildContext context) => classname(),
+    //'/namerout' : (BuildContext context) => classname(),
+  };
+}
